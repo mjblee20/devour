6e22316edf94122dec9e327bceaa5428d360d421
Monte Cristo <mjblee20@Bills-MacBook-Pro.local>
2019-04-10 22:18:01 -0700

    Working

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "devour_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "devour_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
